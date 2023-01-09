525ab0fecd830efe72a8d89b15d77440eefabd65
Kaviya-Saravanan <kaviya.saravanan@wrkspot.com>
2023-01-09 10:53:14 +0530

    New code

---
+++ src/main/java/com/wrkspot/gettersetter/Demo.java
@@ -0,0 +1,8 @@
+package com.wrkspot.gettersetter;
+
+public class Demo {
+    public static void main(String[] args){
+        System.out.println("Values of students");
+    }
+
+}
---
+++ src/main/java/com/wrkspot/gettersetter/Demo_1.java
@@ -0,0 +1,7 @@
+package com.wrkspot.gettersetter;
+
+public class Demo_1 {
+    public static void main(String[] args){
+        System.out.println("Git add comment");
+    }
+}
